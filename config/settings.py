1d3214897158047a66b7567406ac4b4ed0527592
officialkc1 <kiherome@gamil.com>
2021-11-30 16:07:34 +0100

    To-do api

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('MY_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['my-todo1-api.herokuapp.com']
 
 
 # Application definition
